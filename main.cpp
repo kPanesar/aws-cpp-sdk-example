446d367d6935f21b75e49ebf3599744f05c9d3cc
Karanvir Panesar <singh.karanvir21@gmail.com>
2017-01-18 22:13:26 -0800

    Added functionality to get bytes from Poilly AudioStream using dynamic sizing.

--- main.cpp
+++ main.cpp
@@ -23,8 +23,8 @@
 
 void TestDynamoDB();
 void TestPolly();
-
-using namespace std;
+int GetStreamSize(Aws::IOStream* stream);
+char* GetStreamBytes(Aws::IOStream* stream);
 
 int main(int argc, char *argv[])
 {
@@ -58,10 +58,10 @@ void TestDynamoDB()
 
     // Process the results of the request
     if(putItemOutcome.IsSuccess()){
-        cout << "PutItem Success Using IOPS " << putItemOutcome.GetResult().GetConsumedCapacity().GetTableName() << "\n";
+        std::cout << "PutItem Success Using IOPS " << putItemOutcome.GetResult().GetConsumedCapacity().GetTableName() << "\n";
     }
     else{
-        cout << "PutItem failed with error " << putItemOutcome.GetError().GetMessage() << "\n";
+        std::cout << "PutItem failed with error " << putItemOutcome.GetError().GetMessage() << "\n";
     }
 }
 
@@ -73,22 +73,45 @@ void TestPolly()
     speechRequest.SetVoiceId(Aws::Polly::Model::VoiceId::Kendra);
     speechRequest.SetOutputFormat(Aws::Polly::Model::OutputFormat::mp3);
 
-    speechRequest.SetText("Hello Dave, how are you doing today?");
+    speechRequest.SetText("Flight 1 2 7 is now headed to Denver. Please make appropriate adjustments.");
     auto result = pollyClient.SynthesizeSpeech(speechRequest);
 
-    //Hard-coded size temporarily
-    streampos size = 128000;
-    char* memblock = new char [size];
-    result.GetResult().GetAudioStream().read(memblock, size);
-
-    ofstream voiceFile ("voice.mp3");
-    voiceFile.write(memblock, size);
-
     if (result.IsSuccess()){
-        cout << "Speech synthesis was successfull.";
+        std::cout << "Speech synthesis was successfull. Saving to file now..." << std::endl;
+
+        Aws::String     file_name = "polly.mp3";
+        Aws::IOStream*  audio_stream = &result.GetResult().GetAudioStream();
+        Aws::OFStream   voice_file;
+
+        // Get the bytes from the stream and save them into a file
+        voice_file.open(file_name.c_str(), std::ios::out | std::ios::binary);
+        voice_file.write(GetStreamBytes(audio_stream), GetStreamSize(audio_stream));
+        voice_file.close();
+
+        std::cout << "Done!" << std::endl;
     }
     else{
-        cout << "Speech synthesis failed.\n";
-        cout << "Error: " << result.GetError().GetMessage();
+        std::cout << "Speech synthesis failed.\n";
+        std::cout << "Error: " << result.GetError().GetMessage();
     }
+}
+
+int GetStreamSize(Aws::IOStream* stream){
+    // Ensure the stream is at the beginning
+                stream->seekg (0, std::ios::beg);
+    int begin = stream->tellg();
+                stream->seekg (0, std::ios::end);
+    int end =   stream->tellg();
+                stream->seekg (0, std::ios::beg);
+
+    return end-begin;
+}
+
+char* GetStreamBytes(Aws::IOStream* stream){
+    // Read the stream bytes into a memory block
+    int size = GetStreamSize(stream);
+    char* bytes = new char[size];
+    stream->read(bytes, size);
+
+    return bytes;
 }
